e4f4885ae2f04c36f03444a23c9ed0e004d1f646
thomas mayrhofer <thomas@mayrhofer.at>
2014-05-12 11:59:24 +0200

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('qapiFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
