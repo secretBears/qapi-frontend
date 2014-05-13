5ac4c8c6f5b94868a94183588902f0c90f99f09e
vagrant <vagrant@rails-starter-box>
2014-05-13 13:40:20 +0000

    create game model and give a statistic to the user after answer all qestions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,42 +4,7 @@ angular.module('qapiFrontendApp').controller('MainCtrl', ['$scope', '$http', '$w
 	function ($scope, $http, $window, Game) {
 
 		$scope.game = Game;
-		$scope.questioncount = 1;
-		$scope.game.numberofquestions = 10;
-		$scope.game.rightQuestions = 0;
 
-		$scope.getNewQuestion = function(){
-			$http({method: 'GET', url: 'http://qapi.herokuapp.com/api/'})
-		    .success(function(data) {
-		      $scope.qapi = data;
-		    })
-		    .error(function() {
-		      console.log('ERROR: fetching data from QAPI');
-		      //TODO: remove fallback
-		      $scope.qapi = {'id':20,'question':'Frage 20','place':'Linz', 'answers':[{'answer':'20 answer 1','isTrue':false},{'answer':'20 answer 2','isTrue':true},{'answer':'20 answer 3', 'isTrue':false},{'answer':'20 answer 4','isTrue':false}]};
-				});
-		};
-
-		$scope.giveAnswer = function(answer){
-			console.log(answer);
-			//check Answer
-			for(var i=0; i<$scope.qapi.answers.length; i++){
-				var a = $scope.qapi.answers[i];
-				if(a.answer === answer && a.isTrue){
-					$scope.game.rightQuestions++;
-					break;
-				}
-			}
-
-			if($scope.questioncount < $scope.game.numberofquestions){
-				$scope.getNewQuestion();
-				$scope.questioncount++;
-			}
-			else{
-				$window.location.href = '/#/finish';
-			}
-		};
-
-		$scope.getNewQuestion();
+		$scope.game.getNewQuestion();
 	}
   ]);
--- app/scripts/models/game.js
+++ app/scripts/models/game.js
@@ -1,5 +1,67 @@
 'use strict';
 
-angular.module('qapiFrontendApp').factory('Game', function(){
-	return {};
-});
+angular.module('qapiFrontendApp').factory('Game', ['$http', '$window', function($http, $window){
+	var instance;
+
+	var Game = function Game(){
+		this.numberofquestions = 10;
+		this.rightQuestions = 0;
+		this.givenAnswers = [];
+		this.questioncount = 0;
+		this.question = {};
+	};
+
+	Game.prototype.getNewQuestion = function(){
+		var scope = this;
+
+		$http({method: 'GET', url: 'http://qapi.herokuapp.com/api/'})
+	    .success(function(data) {
+	      scope.question = data;
+	    })
+	    .error(function() {
+	      console.log('ERROR: fetching data from QAPI');
+	      //TODO: remove fallback
+	      scope.question = {'id':20,'question':'Frage 20','place':'Linz', 'answers':[{'answer':'20 answer 1','isTrue':false},{'answer':'20 answer 2','isTrue':true},{'answer':'20 answer 3', 'isTrue':false},{'answer':'20 answer 4','isTrue':false}]};
+			});
+	};
+
+	Game.prototype.giveAnswer = function(answer){
+		var givenAnswer = {};
+		givenAnswer.question = this.question.question;
+		givenAnswer.answer = answer;
+
+		//check Answer
+		var isTrue = false;
+		for(var i=0; i<this.question.answers.length; i++){
+			var a = this.question.answers[i];
+			if(a.isTrue){
+				givenAnswer.rightAnswer = a.answer;
+			}
+			if(a.answer === answer){
+				isTrue = a.isTrue;
+			}
+		}
+
+		givenAnswer.isTrue = isTrue;
+
+		this.givenAnswers.push(givenAnswer);
+
+		if(isTrue){
+			this.rightQuestions++;
+		}
+
+		if(this.questioncount < this.numberofquestions - 1){
+			this.getNewQuestion();
+			this.questioncount++;
+		}
+		else{
+			$window.location.href = '/#/finish';
+		}
+	};
+
+	if(!instance){
+		instance = new Game();
+	}
+
+	return instance;
+}]);
