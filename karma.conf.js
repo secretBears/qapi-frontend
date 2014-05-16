1eba0206a76244d1f705d4aad1a20a184bc85f15
vagrant <vagrant@rails-starter-box>
2014-05-16 07:43:06 +0000

    add angular-loading-bar to bower. add angular-loading-bar model to karma config (test)

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-loading-bar/src/loading-bar.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
 //      'test/mock/**/*.js',
